1b1969412310ce8a948e8a97db8eacf8939287ce
Golla Satheesh <gollasatheesh95@gmail.com>
2026-03-31 22:45:04 +0530

    Add files via upload
    This programe shows slicing in tuples and explaining it with simple progemme

---
+++ Day-12/Q4.py
@@ -0,0 +1,5 @@
+
+'''slicing in tuple means accessing the portion of elements '''
+m=("anantapur","tirupati","Puttur","Kalyandurg","bangalore")
+print("printing 0 to 1 :",m[0:1])
+print("printing 1 to 3 :",m[1:3])
