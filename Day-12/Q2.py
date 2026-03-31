dccbfbe3ba9a1bfea2a2b749e38236efb2fac9d0
Golla Satheesh <gollasatheesh95@gmail.com>
2026-03-31 22:41:20 +0530

    Add files via upload
    The difference between list and touple and displaying it with examples

---
+++ Day-12/Q2.py
@@ -0,0 +1,8 @@
+'''1.List is a mutable and ordered collection of elements , And we dynamically change
+    the elements after creation
+   2.Tuple is a ordered and immutable collection of elements and we cant change the 
+   elements after creation'''
+list1=[1,2,3,4,5]
+Tuple1=(6,7,8,9,10)
+print("List:",list1)
+print("Tuple:",Tuple1)
