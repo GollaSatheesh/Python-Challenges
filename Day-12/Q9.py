795ac9673f4a49ce6918973664ee23b01c2f5efa
Golla Satheesh <gollasatheesh95@gmail.com>
2026-03-31 22:50:31 +0530

    Add files via upload
    A program to explain tuple packing and unpacking

---
+++ Day-12/Q9.py
@@ -0,0 +1,17 @@
+'''tuple packing is nothing but grouping multiple values into a single tuple and 
+the values should be seperated by comma'''
+t = (10,20,30,40,50)
+print(t)
+print(type(t))
+#-----------------------------------------------------------------------
+'''Tuple unpacking is nothing but reverse process of tuple packinng called assigning 
+multiple values to individual variables'''
+
+tu=("anantapur","tirupati","Puttur","Kalyandurg","bangalore",1)
+a,b,c,d,e,f=tu
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
