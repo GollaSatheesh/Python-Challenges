1aaa1e262538bb9733768f9fd04bc3537954e440
Golla Satheesh <gollasatheesh95@gmail.com>
2026-03-31 22:43:01 +0530

    Add files via upload
    This shows how  access elements in a tuple and demonstrating it with program

---
+++ Day-12/Q3.py
@@ -0,0 +1,5 @@
+'''we can directly access the elements using INDEX keyword'''
+m=("anantapur","tirupati","Puttur","Kalyandurg")
+print("1st element:",m[0])
+print("2nd Element:",m[1])
+print("3rd element:",m[2])
