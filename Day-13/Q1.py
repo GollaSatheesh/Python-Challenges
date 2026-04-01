d700c79794c9fe44158557587b4e12b9a85ccf32
Golla Satheesh <gollasatheesh95@gmail.com>
2026-04-01 22:38:37 +0530

    Create Q1.py
    Add a Python script demonstrating set usage.

---
+++ Day-13/Q1.py
@@ -0,0 +1,7 @@
+'''If we want to store multiple objective elements in single entity then we can 
+use set and it wont accept duplicates if we create duplicate it automatically 
+considers it as a single object and it wont stores order of insertion it uses 
+hash principle to store data'''
+
+s={10,20,30,40,50}
+print(s,"and",type(s))
