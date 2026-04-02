3d2ad2fde8bdd6921d352681d5c56c3258289801
Golla Satheesh <gollasatheesh95@gmail.com>
2026-04-02 21:55:37 +0530

    Delete Day 14/Q4.py,Q5.py,Q6.py

--- Day 14/Q4.py
+++
@@ -1,6 +0,0 @@
-'''If we want to create empty set we should use "set()" and it stores values using {} 
-this only creates empty dictionary and dictionary stores pairs of values'''
-emptySet=set()#only one way to create empty set
-emptyDict={}#If we use empty {} then we can create empty dictionary
-print(emptySet,"and",type(emptySet))
-print(emptyDict,"and",type(emptyDict))
