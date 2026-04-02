f95ee2e0ee59ccd4a2e7032372bb1916258e89e9
Golla Satheesh <gollasatheesh95@gmail.com>
2026-04-02 21:11:48 +0530

    Add files via upload

---
+++ Day 14/Q2.py
@@ -0,0 +1,9 @@
+'''Union is mathematical operation in set If we have two sets and having elements
+it print the both elements of the sets by considering twice occured element as once'''
+a={1,2,3,4}
+b={4,5,6,7}
+print(a|b,"AND",a.union(b))
+
+'''Intersection is a mathematical operation in set, it prints only the which are in 
+both sets '''
+print(a&b,'and',a.intersection(b))
