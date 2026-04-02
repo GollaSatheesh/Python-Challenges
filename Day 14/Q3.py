e992ea16be5a565a6d2e696e5768db9b51667554
Golla Satheesh <gollasatheesh95@gmail.com>
2026-04-02 21:54:56 +0530

    Delete Day 14/Q3.py

--- Day 14/Q3.py
+++
@@ -1,3 +0,0 @@
-'''To create an empty set we have built in function called "set()" function'''
-s=set()#we actully represet set with "{}" but if we use flowers brackets , it denotes as a dictionary
-print(s,"and",type(s))
