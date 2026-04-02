6f9cefde334f4ab2c3e437614b401db80147cbd5
Golla Satheesh <gollasatheesh95@gmail.com>
2026-04-02 21:56:09 +0530

    Delete Day 14/Q6.py

--- Day 14/Q6.py
+++
@@ -1,6 +0,0 @@
-'''Using update method we can update elements , where we elements can be iterable like list,tuple'''
-Set=set()
-List=[1,2,3,4,5]#List is a varibale not list function
-Tuple=("Satheesh","Karthik","Akruthi")#Here also
-Set.update(List,Tuple)#Parameters directly updating to the Set elements from "Tuple","List"
-print(Set)
