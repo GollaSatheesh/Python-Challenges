bbad2fc4a4c1012dc04f3b19ad0360b01e391ec5
Golla Satheesh <gollasatheesh95@gmail.com>
2026-04-04 19:46:17 +0530

    Add.py to generate odd numbers dictionary
    Create a dictionary of odd numbers from 0 to 20 using comprehension.

---
+++ Day 16/Q3.py
@@ -0,0 +1,4 @@
+'''Dict supports the comprehension for example we will printing odd numbers upto 20
+using range function and loop'''
+Dictionary={i for i in range(0,20,1) if i%2!=0}
+print(Dictionary)
