7e0a3e8be81343718f43342f3db2efea3dfb985d
Golla Satheesh <gollasatheesh95@gmail.com>
2026-04-04 19:44:52 +0530

    Implement dictionary example with key-value access
    Added a dictionary example demonstrating how to access keys, values, and items.

--- Day 16/Q1.py
+++ Day 16/Q1.py
@@ -1,1 +1,15 @@
+'''Dictionary is collection of items which stores the in formate of keys and values ,
+  if we want to access keys there is inbuilt method called "DictionaryName.keys()" and
+  if we want to access values same like keys ,"DictionaryName.values()" and if we want
+  access keys and values (They are also called items) want to acces them "DictionaryName.items()"
+  '''
+d={
+    "Name":"Satheesh",
+    "Pursuing":"Data scientist"
+   }
+print("The values in the Dictionary:",d.values())
+print("The keys in the Dictionary:",d.keys())
+print("The items in the Dict:",d.items())
 
+for keys,values in d.items():
+        print(keys,"->",values)
