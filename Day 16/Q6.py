54d137ace084122e0bfd2ce4b8172909f1e1586e
Golla Satheesh <gollasatheesh95@gmail.com>
2026-04-04 19:48:52 +0530

    Initialize dictionary with personal information
    Add a dictionary with personal details and set default city

---
+++ Day 16/Q6.py
@@ -0,0 +1,8 @@
+d={
+    "Name":"Satheesh",
+    "Pursuing":"Data scientist",
+    "Learning":"Python",
+    "Age":"22"
+   }
+d.setdefault('city','Unknown')#Here the default value is unknow that was given by me or it will take default value as none
+print(d)
