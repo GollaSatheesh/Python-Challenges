9e5a4e3b42fcb4eaac04e77ab2288f4f03726a5c
Golla Satheesh <gollasatheesh95@gmail.com>
2026-04-04 19:48:23 +0530

    Create Q5.py to show dictionary copy behavior
    Add a script to demonstrate dictionary copying in Python.

---
+++ Day 16/Q5.py
@@ -0,0 +1,12 @@
+dictionary={
+    1:'Satheesh',
+    2:'Charan',
+    3:'Chandu',
+    4:'Karthik',
+    5:'Sameer'
+}
+dictionary2=dictionary.copy()
+print("The original dictionary :",dictionary)
+print('The original dictionary id :',id(dictionary))
+print('The copied dictionary is :',dictionary2)
+print('The id of the copied dictionary',id(dictionary2))
