df654a1e4590f8d53d49fb4f1aa1e9b6a8d3588e
Golla Satheesh <gollasatheesh95@gmail.com>
2026-04-04 19:49:57 +0530

    Add personal info dictionary and print items
    Added a dictionary with personal information and printed its keys and values.

---
+++ Day 16/Q8.py
@@ -0,0 +1,8 @@
+d={
+    "Name":"Satheesh",
+    "Pursuing":"Data scientist",
+    "Learning":"Python",
+    "Age":"22"
+   }
+for keys,values in d.items():#Printing all keys and values through iteration
+    print(keys,"->",values)
