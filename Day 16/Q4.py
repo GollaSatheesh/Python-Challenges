7fceca3c760ab5b129d01a6bbf9ce9ab349e24fc
Golla Satheesh <gollasatheesh95@gmail.com>
2026-04-04 19:46:56 +0530

    Add dictionary example with keys, values, and items

---
+++ Day 16/Q4.py
@@ -0,0 +1,10 @@
+dictionary={
+    1:'Satheesh',
+    2:'Charan',
+    3:'Chandu',
+    4:'Karthik',
+    5:'Sameer'
+}
+print('All keys present in the Dictionary :',dictionary.keys())
+print('All values present in the Dictionary :',dictionary.values())
+print('All items present in the Dictionary',dictionary.items())
