4cdd706c98215cfd85430524e8643f230e73c533
Golla Satheesh <gollasatheesh95@gmail.com>
2026-04-04 19:49:25 +0530

    Create and update dictionary
    Added a dictionary and updated it with another dictionary.

---
+++ Day 16/Q7.py
@@ -0,0 +1,9 @@
+d1={
+    "Name":"Satheesh",
+    "Pursuing":"Data scientist",
+    "Learning":"Python",
+    "Age":"22"
+   }
+d2={'place':'Avulenna'}
+d1.update(d2)#It updates the d1 elements with d2 elements
+print(d1)
