1aa793f23e49e55ef73ee77f9f5f66e629e8d97c
Golla Satheesh <gollasatheesh95@gmail.com>
2026-04-07 21:59:30 +0530

    Add dictionary of persons with details for  example of nested collections

---
+++ Day 17/Q7.py
@@ -0,0 +1,7 @@
+d1={
+    "person1":{"marital status":None,"name":"madhu","Age":22},
+    "person2":{"marital status":"Yes","name":"Karthik","Age":"23"},
+    "person3":{"marital status":"No","name":"Charan","Age":"23"}
+}
+for key,value in d1.items():
+    print(key,"->",value)
