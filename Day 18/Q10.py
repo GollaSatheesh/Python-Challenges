e0233b3e88ba3a1b7779217c282e3422bf91ffe7
Golla Satheesh <gollasatheesh95@gmail.com>
2026-04-08 22:02:19 +0530

    Printing Largest across Three Numbers

---
+++ Day 18/Q10.py
@@ -0,0 +1,7 @@
+def Largest():
+    a=int(input("Enter the First Number : "))
+    b=int(input("Enter the Second Number: "))
+    c=int(input("Enter the Third Number : "))
+    print(f"The Largest Number is {max(a,b,c)}...")
+
+Largest()
