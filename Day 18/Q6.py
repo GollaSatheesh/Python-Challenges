7aa7815d9b9fcce5741c6e54a03d39396ca8640a
Golla Satheesh <gollasatheesh95@gmail.com>
2026-04-08 21:57:14 +0530

    Example code for without input but output there

---
+++ Day 18/Q6.py
@@ -0,0 +1,8 @@
+def WithOutArgument():
+    a=int(input("Enter the first number : "))
+    b=int(input("Enter the second number : "))
+    c=a+b
+    print(f"The Sum of {a} and {b} is {c}")
+    return c
+
+WithOutArgument()
