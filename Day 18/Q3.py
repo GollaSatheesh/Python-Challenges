f5ec0a6df93df0d90f29e39c94eec6bc4ffb0c66
Golla Satheesh <gollasatheesh95@gmail.com>
2026-04-08 21:51:49 +0530

    Add documentation for formal and actual parameters

---
+++ Day 18/Q3.py
@@ -0,0 +1,7 @@
+#Formal parameters
+'''The formal parameters means which are variables defined in a definition of the funtion
+they can be optional also'''
+
+#Actual parameters
+'''The Actual parameters are the real values , they expected input in definition of the 
+function'''
