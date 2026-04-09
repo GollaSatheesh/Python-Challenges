f1b84e05011b777dfa7c679aeb1f647d2ea654a4
Golla Satheesh <gollasatheesh95@gmail.com>
2026-04-09 22:24:36 +0530

    Variable length arguments and their types

---
+++ Day 19/Q2.py
@@ -0,0 +1,8 @@
+'''Variable length arguments means if we dont exactly how much varables will be using in the 
+   function then and we can pass any number of arguments in the function in Variable length 
+   arguments.They are 2 types
+      1.keyword variable lenth arguments 
+          We can use this by "*Args* with this by passing in the parameters we can only define 
+          single values not key value pairs in function definition
+      2.Non-Keyword variable lenth arguments
+          We can us this to pass key value pair arguments by using "**" Symbol'''
