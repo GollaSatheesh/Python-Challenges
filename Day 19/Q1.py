8a0366573cf29d6c16cb4512b716643dd2713890
Golla Satheesh <gollasatheesh95@gmail.com>
2026-04-09 22:22:49 +0530

    What are Default arguments in Python functions

---
+++ Day 19/Q1.py
@@ -0,0 +1,3 @@
+'''Default arguments in the function concept is to print default which we have passed in the 
+function arguments , But this executes if  we didnt passed any in actual parameters then 
+default value will be executed'''
