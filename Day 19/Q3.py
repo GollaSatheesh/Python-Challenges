32eed96c2122e77e419e097ff85ef0361131ed4a
Golla Satheesh <gollasatheesh95@gmail.com>
2026-04-09 22:25:32 +0530

    Difference between local variables and global variables

---
+++ Day 19/Q3.py
@@ -0,0 +1,8 @@
+'''1.Local Variable
+        This can be created only inside the funtion and that can be used and called inside 
+        the function only , They cant be accessed outside the function
+    2.Global Variable
+        This can be created outside the function  and also inside the function by using 
+        "Global" Keyword . This value can be whereever you want inside the function and outside
+        the function but if we want access in the function first priority will be for local
+        varibales first.'''
