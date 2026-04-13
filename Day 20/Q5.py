ef156d2d27dd48d249ca0d28df3917ffd123dece
Golla Satheesh <gollasatheesh95@gmail.com>
2026-04-13 19:09:12 +0530

    A program to create a lambda function to find the square of a number

---
+++ Day 20/Q5.py
@@ -0,0 +1,8 @@
+S=int(input("Enter the number:"))
+w=(lambda S:S*S)
+print(w(S))# w is the lambda function and S is the value
+
+
+print("##########################################################")
+s=(lambda n:n*n)
+print(s(100))#this is the syntax to pass value to lambda function
