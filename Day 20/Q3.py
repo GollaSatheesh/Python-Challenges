a8447d00da0228a7762b6a40aeab30bbb0a5b478
Golla Satheesh <gollasatheesh95@gmail.com>
2026-04-13 19:06:41 +0530

    The between a normal function and a lambda function

---
+++ Day 20/Q3.py
@@ -0,0 +1,8 @@
+'''1.In lamda functions we have to write code in single only only , lamda function cant accept muliple
+    lines of code
+   2.In normal python function we can write multiple lines of code it accepts multiple lines 
+     of code
+   3.In lamda function we will define it with keyword "Lamda" 
+   4.In normal function we will define it with "def" keyword
+   5.In lamda functions it auto returns the result 
+   6.In normal python function that is optional '''
