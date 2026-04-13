61c711673265ea7b209bb677e22331253d245334
Golla Satheesh <gollasatheesh95@gmail.com>
2026-04-13 19:05:57 +0530

    What is a lambda function in Python

---
+++ Day 20/Q2.py
@@ -0,0 +1,4 @@
+'''1.We can create lamda function for single line of code for calculations.
+   2.Lamda functions can be called as anonymous functions
+   3.Lamda cant be reusble as noraml functions in python
+   4.In Lamda functions we cant write multiple lines of code the should be in single line only '''
